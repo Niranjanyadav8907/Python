9e50592301524869b093d2ebda260c9c7bbb2bc3
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-10 22:43:39 +0000

    finished,

---
+++ first1.py
@@ -0,0 +1,6 @@
+print("hello world") # string- collection of character inside "double quotes" or 'single quotes'
+print('hello world') 
+print("hello'world' world") # you can use 'single quotes' inside "double quotes" 
+print('hello "world" world') #  or "or' single quotes' "doubel quotes " likh skte hain
+# single quotes ke ander singel quotes nhi likh skte hain
+print("i'm niranjan") #aise likh skte hain
