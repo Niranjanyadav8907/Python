93796a1268ad2a5c168c5e78a766ee44841a790f
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-10 23:22:24 +0000

    Done7

---
+++ escape_sequence.py
@@ -0,0 +1,5 @@
+print("hello \"world\' world") # \ 'single quote
+print("line A\nline B") # \n   new line 
+print("name\tniranjan") # \t tab 
+print("this is backslash\\") #\\ backslash
+print("hell\blo") # \b backspace
