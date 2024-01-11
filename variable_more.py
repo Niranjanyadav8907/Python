b4d6b2546bb6f87166063d8d7748bfcb1e4472ae
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-11 18:10:54 +0000

    More about variables 20

---
+++ variable_more.py
@@ -0,0 +1,7 @@
+name, age = " Niranjan", "22"
+print("hello" + name + "your age is " + age) # ek hi line me bahut sare value aise likhte hain
+
+x=y=z=1
+print(x+y+z) # ek line multible variable aise likh skte hain
+
+
