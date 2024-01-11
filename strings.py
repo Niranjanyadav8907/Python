7acef44b389610d6f5e0fc941361e9e22265361c
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-11 17:20:48 +0000

    Done 17

---
+++ strings.py
@@ -0,0 +1,14 @@
+#string 
+# collection of characters incide singes quottes or double quotes
+
+first_name = "Niranjan" 
+last_name  = "Kumar"
+full_name = first_name +" "+ last_name
+print(full_name)
+print(first_name + "3")
+print(full_name + str(3))
+
+#print(first_name + 3) error
+#print(first_name + "3")  no error
+#print(first_name + str(3)) #no eror
+
