d112822f492cc8e963e2ad7db2b1d63afd5c4a39
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-11 17:58:31 +0000

    2 number sum 19

---
+++ int_func.py
@@ -0,0 +1,11 @@
+number_one = int(input("enter first number"))
+number_two = int(input("enter second number"))
+total = number_one + number_two 
+print("total is " + str(total))
+
+# total = "4" + "4" = 44
+# str----- "4" aise change kr deta hain
+
+# int fun-----"4" me  4 change krta hai
+
+# float "4" ko 4.0 me change kr deta hai
