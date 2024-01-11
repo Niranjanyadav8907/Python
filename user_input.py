520814dfa29fc7a52dec32ffa3bc28df42594db2
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-11 17:38:22 +0000

    Done 18

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#user input 
+# input function
+name = input("type your name")
+print("hello" + name) # input fun hmesha user input string me hi leta hain
+#example
+age = input ("what is your age ? ") #  user keybord me number input kiya but te strige me hi save huaa code me "22" aise hi save huaa hoga 
