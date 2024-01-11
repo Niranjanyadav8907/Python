3b81f44705c7af122b4ca5420fef386dc91d9f13
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-11 18:24:42 +0000

    Two or more input in one line

---
+++ two_inputs.py
@@ -0,0 +1,8 @@
+#name = input("enter your name : ")
+#age = input ("enter you age : ")
+
+name, age = input("enter your name and age").split()
+print(name)
+print(age)
+ # input ko space dalna pdhega 
+#spece ko hmm change kr skte ---- split(",") aise likh kr
