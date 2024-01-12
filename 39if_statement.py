92d6ae3ff6fc7e3716fcfad9cd42f6751ba152c2
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 19:55:55 +0000

    If statement

---
+++ 39if_statement.py
@@ -0,0 +1,11 @@
+#syntax
+#if condition :
+# code # if candition is true then this code will execute
+
+# if sta canditione ko check krta sahi hsi ya glt
+
+age = input( "enter youre age :")
+age = int (age)
+if age >= 14:
+    print ("you are above 14") # {} python me iske jgh pr : use hota hain
+
