52f9d1cb0362593fe405e982163a407e768cae62
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 09:28:17 +0000

    String Formatting

---
+++ 22formatting.py
@@ -0,0 +1,24 @@
+name= "Niranjan"
+age = 22
+print("hello " + name + "your age is " +str(age)) #ugly syntax
+
+#python me string formating 3type ke hote hain
+#python 2
+#python 3
+#python 3.6 (best)
+
+print("hello {} your age is {} ". format(name,age)) # python 3
+#placeholder ka matlab curly brackets {}
+
+# 3.6
+print (f"hello {name} your age is {age}") #clean
+
+#isme add bhi krr skte hai ya +,- kuchh bhi kr skte hain 
+#example--- { age + 2}, {age -2} 
+
+
+
+
+
+
+
