8827308ec0fdb60dbcb36f60d2aa5895f9a7f290
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 19:06:38 +0000

    Center method

---
+++ 35center_method.py
@@ -0,0 +1,9 @@
+# center method 
+name = "Niranjan"
+#**Niranjan** , 12 sb se pahle string lenth ko count krte hai phir jitna add krna rhta hai use se jyda likhte hain 
+
+print(name.center(12,"*"))
+
+name = input("enter your name :")
+print(name.center(len(name)+8, "*")) # user dvra diya gya input ke dono side baraber * print krne kelie kitne bhi lent ka inout ho skta hai
+
