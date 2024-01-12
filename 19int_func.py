97d828c8a6bcf19d7d7204b3877af9c9e0c34a0b
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 23:54:26 +0530

    Update 19int_func.py

--- 19int_func.py
+++ 19int_func.py
@@ -6,6 +6,6 @@ print("total is " + str(total))
 # total = "4" + "4" = 44
 # str----- "4" aise change kr deta hain
 
-# int fun-----"4" me  4 change krta hai
+# int fun-----"4" ko  4 me change krta hai
 
 # float "4" ko 4.0 me change kr deta hai
