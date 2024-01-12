8b23dd7447d311ef4214970828714a0cf3b1495d
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 13:36:06 +0000

    Step Arugumet

---
+++ 27string_slicing.py
@@ -0,0 +1,10 @@
+#slicing / selecting sub sequences 
+#lang = "python"
+# syntax - [start argument : stop argument -1 : step]
+
+print("Niranjan"[0:5:2])  # step ka mtlab aap chitna step denge utna chhod chhod kr print krega  
+
+#back jane keliye [-1]
+print("Niranjan" [8 : :-1])  # is se hmm string revers bhi kr skte hain
+
+print("niranjan"[:: -1]) # trick string revers ka 
