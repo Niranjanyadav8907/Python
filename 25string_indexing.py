6e44d88cd52eaec212ca1ae618ea895c84d07336
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 12:59:52 +0000

    String Indexing

---
+++ 25string_indexing.py
@@ -0,0 +1,18 @@
+#string indexing
+language = "python"
+
+#positions(index number)
+
+# p = 0 , -6
+# y = 1 , -5
+# t = 2 , -4
+# h = 3 , -3 
+# o = 4 , -2
+# n = 5 , -1
+
+#print(language[2]) # python me index number likhne kelie hmesh [] hi lgate hain
+
+# kisi prticuler number print krane kelie indexing ka use krte hain
+print(language[-1]) #last number print krane keliye - ka use krte hain (ULTA SIDE SE NUMRING KRTE HAIN)
+
+# Start hmesa 0 se krte hain or ulata jb count krte hai to -1 se
