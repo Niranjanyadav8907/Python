f0579483827d69c31eb7f94662b60d5e662200ec
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 14:15:26 +0000

    String Methode part 1

---
+++ 30string_mehtods.py
@@ -0,0 +1,25 @@
+#string mehtods part one 
+
+name = "Nranjan yadav"
+
+#1. len() function
+#print(len(name)) # lent spce ko bhi count 
+length = len(name) 
+print(length) # ise hmm vairible bna kr  bhi stor kra skte hai
+
+
+
+#2. lower() method
+print(name.lower()) # method me hmm . ka use  krte hain 
+
+#3. upper() method
+print(name.upper())
+
+
+#4. title() method ---- title method bs pahle chr ko capitel me likhta hain
+print(name.title())
+
+#5. count() method --- coumt method string me ek (same char) kitni baar hain count krta hain
+print(name.count("n"))
+
+
