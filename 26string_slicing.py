dadd8d85474cf8e0c60cd8b59ba2fd6f96dbf415
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 13:18:15 +0000

    string slicing

---
+++ 26string_slicing.py
@@ -0,0 +1,11 @@
+#slicing / selecting sub sequences
+lang = "python"
+# ek se adhik char  ko print krane keliye slicing ka use krte hain
+# print(lang[4]) 
+# syntax - [start argumet : stop argument -1 ] ek extra likhte hain
+
+print(lang[0:2]) 
+
+#print(lang[:]) # pura string print krane keliye 
+
+
