c176500146ae80c89515a5fe123b3f06d2acf4ec
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-12 17:40:14 +0000

    Exercise

--- 31exercise.py
+++ 31exercise.py
@@ -1,3 +1,4 @@
+#  question
 # take two comma saparted input from user 
 # 1 user's name , example - "Niranjan"
 # 2 a single character, 
@@ -13,3 +14,12 @@ print(f"lenth of your name is {len(name)}")
 # Niranjan- niranjan
 print(f"charcter count : {name.lower().count(char.lower())}") # case sensitive ko count krne keliye 
 # N - n
+
+
+#question
+# "   Niranjan   "___________>"Niranjan"___________phir lower alfa me change krenge "niranjan"
+# same work kisi chr keliye bhi kr ke dekhte hai 
+#"   H  "______space remove______________"H"_________lower change_________h
+#name.strip().lower().count(char.strip().lower()) # ye line number 15 me {past krna hai or check kr skte hain
+
+
