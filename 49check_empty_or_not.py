94683ad0f8bbd0ee3f3c9f91538ea5e5f5d3c5ac
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 10:58:51 +0000

    Check Empty

---
+++ 49check_empty_or_not.py
@@ -0,0 +1,26 @@
+# check empty or not # important name =
+#imortent
+
+
+name = "" # ager string me kuchh nhi hai to empty bolte hai or string ke andr kuchh hain  to string is not empty print hoga 
+
+if name: # true if string is not empty
+
+ print("string is not empty")
+
+else:
+
+  print("string is empty")
+
+
+
+name = input ("enter the name :") # user bhi check kr skta hain empty 
+
+if name: 
+  print(f" your name is {name}")
+else:
+  print("you didt\'t type anything" )
+
+
+
+
