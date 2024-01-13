6d6e2cc9cae05fbe7310364e48065eddef30da28
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 09:32:12 +0000

    And, or Candition

---
+++ 44and_or.py
@@ -0,0 +1,20 @@
+#check two canditions at same time
+# and ,or two canditions check krne keliye
+
+name = 'abc'
+age = 19
+if name == 'abc' and age == 19: # and candtion me dono candtion true hona chahiye tbhi true hoga 
+    print("condition True" )
+else:
+    print("condition false")
+    
+
+
+#or candition
+name = 'abc'
+age = 19
+if name == 'abc' or age == 34: # or candtion me ek bi  candtion true hain to condition true ho jayga 
+    print("condition True" )
+else:
+    print("condition false")
+    
