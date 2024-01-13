5a23ea5dee63440568c399723aa230eaf3800ccd
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 17:55:40 +0000

    Infinite loop

---
+++ 58infinite_loop.py
@@ -0,0 +1,20 @@
+#infinite loop
+#infinite loop two type ke hote hai jo glti se chl jata hain or ek jarut ke sath jo chlate hain
+
+i = 0
+while i <= 10:
+    print("Niranjan kumar yadav")
+    #i = i + 1 i ke value ko incriase difine krna bhul gy 
+    # ise hmm rokne keliye [[ctrl + c]] use krte hain
+
+
+ 
+ # jan kr chlane keliye 
+    # while true:
+      #print("Niranjan kumar yadav")
+    
+
+#python me two bolean hota hain
+    # 1 True
+    # 2 False
+    
