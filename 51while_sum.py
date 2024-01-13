e305e60982a46856743cd627551127868bb6d552
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 16:05:45 +0000

    Sum of Numbers Program using while loop

---
+++ 51while_sum.py
@@ -0,0 +1,11 @@
+# sum : 1 to 10 (or any number)___ Q while loop use krke 1 se 10 tk sum kre 
+
+total =  0
+i = 1  # i = 2 
+while i <= 10:
+    total = total + i #total = total +i ko aap aise bhi likh skte hain  (total+= i)
+    i = i + 1
+print(total)
+
+
+    #total = 0 + 1 + 2 + 3 + 4 aise hi add hote jayga
