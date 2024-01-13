2199b5ae5b85d2e47d80011494d854512347499c
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 08:47:02 +0000

    If-else staement

---
+++ 41else_statement.py
@@ -0,0 +1,8 @@
+age = input( "enter youre age :")
+age = int (age)
+if age >= 14:
+    print ("you are above 14")
+else:
+    print ("sorry, you can't play ")  #else statement hmesa if ke baad likhte hain
+
+    # else statement akele nhi likh skte hain   
