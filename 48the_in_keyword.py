2331391ecf6aff23f3fcb4928c496a58dfe423dd
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 10:36:05 +0000

    in keyword

---
+++ 48the_in_keyword.py
@@ -0,0 +1,11 @@
+name = "Niranjan"
+# in keyword
+# if with in
+
+if 'N' in "Niranjan":
+ print("N is present in name")
+else:
+ print("not present")
+
+
+ #in uase krte hai kisi string me prticuler chr hai ya nhi check krne keliye use krte hain
