1ac43d22224292e630b9deb3f810c41e148b2e5e
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 18:22:43 +0000

    Foor loop example

---
+++ 60for_loop_sum.py
@@ -0,0 +1,23 @@
+# sum from 1 to 10
+
+# 1+ 2+ 3+.......10
+
+
+total = 0
+for i in range(1,11):
+    total += i
+print(total)
+
+
+
+#use input keliye 
+
+n = int(input("Hey Niranjan number input kro :"))
+total= 0
+for i in range(1,n+1): # ye isliye kyu ki usr to normal bs number input krayga 
+    total += i
+print(total)
+
+
+        
+
