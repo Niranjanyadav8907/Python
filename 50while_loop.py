a2f2b63f127ef74065cb4664cd6a58b9f9467113
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 11:19:45 +0000

    while loop

---
+++ 50while_loop.py
@@ -0,0 +1,11 @@
+#loops
+#while loop ,for loop
+
+#print("hello world")  #10 times
+
+i = 1   #  sb se pahle hmm ek vairibel likhte hain kuchh bhi likh skte 1,2,3
+while 1 <=10: # condtione
+  print("hello world ")
+  i = i + 1
+
+
