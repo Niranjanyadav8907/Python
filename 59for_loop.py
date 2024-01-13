6a12853309b65ad37d3183524e73eefad891b675
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-13 18:09:23 +0000

    Foor Loop

---
+++ 59for_loop.py
@@ -0,0 +1,21 @@
+# i = 0
+#while i <= 10
+# print ("hello world")
+# i = i + 1
+
+# for loop with range function-- for loop me range fun use krte hain
+
+for i in range(10):   # i ke jgh pr aap kuchh bhi le skte hain #range 10 mtlb 0 se 9 tk print hoga 
+    print("Niranjan")
+
+
+for i in range(10):  #(1, 11) print hoga 1 to 10
+    print(f"Niranjan : {i}") # kitne baar print ho rha hain numbering ke sath keliye
+    
+
+
+
+
+
+
+
