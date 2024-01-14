934874d4a230910ff97699ffc519953072e0887e
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-14 17:59:54 +0000

    Functions intro

---
+++ 70functione.py
@@ -0,0 +1,44 @@
+# functione -- ka use real life code krne keliye use jyda taivr pr hota hain
+# hmm function me baar baar code nhi likhna pdhta hai ek hi code ko use kr skte hain baar baar
+
+# for example
+name = "Niranjan" # string  ka lenth nikalna hai?
+print(len(name))  # python me devloper ne pahle se lenth functine bna kr diya hain
+
+
+#dekhte hain khud ki functione kaise define krte hain
+# for example -  aap ek aisa fun define krna chahte hai jo do number ko sum krenge
+
+#pythone function ko define krne kelie def key word use krte hain
+
+def add_two(a,b):   #def uske baad jo fun bnana chahte hain uska naam 
+    return a+b
+total= add_two(5,4)
+print(total)
+
+# for exp user dvra to input number sum
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+total = add_two(a,b)                  # fun call 
+print(total)
+
+# string input 
+
+a = (input("enter first name:"))
+b = (input("enter second name:"))
+total = add_two(a,b)                  # fun call 
+print(total)
+
+
+
+# python is  dynamic programming language
+# mtlb functione me aap kuchh bhi input me le skta string, number ,float
+
+
+
+
+
+
+
+
