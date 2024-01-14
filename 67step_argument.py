fd1858d3c3d11068e813e90bfe103eed3a03bb1a
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-14 13:29:51 +0000

    Step argument odd , even

---
+++ 67step_argument.py
@@ -0,0 +1,18 @@
+# step argument mtlb ek number se dusre number tk jane me kitan step le rha hain 
+# 1 to 10 ----1,2,3, 4, step 1
+# 1 to 10------1, 3, 5, step 2
+
+for i in range(1,11,1): #  1 step
+    print(i)
+
+for i in range(1,11,2): #even number 2 step lekr kr skte hain 
+    print(i)
+
+for i in range(0,11,2): # odd number print krane keliye 0 se start or 2 step lekr
+    print(i)
+
+
+#10, 9, 8 aisa print krane keliye ( mtlb ulta print krane keliye )
+    
+for i in range(10,0,-1): # 0 tk jana ho to -1 one likhenge 
+    print(i)
