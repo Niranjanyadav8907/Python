e131fe49916fb080317c43c6f2065409cec93fa0
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-14 13:45:20 +0000

    For loop and string

---
+++ 68for_loop_strings.py
@@ -0,0 +1,23 @@
+# for loop ko strings ke shat kaise use  krenge 
+
+name = " Niranjan"         # is strings ke sbhi chr ko print krana ho to kaise kraynge 
+for i in range(len(name)):
+    print(name[i])
+
+
+
+# python specil tarika
+ 
+name = "niranjan"   
+for i in name:
+    print(i)
+
+#example krte hain 
+#1256....> 1+2+5+6
+    
+num = input("enter a number :")
+total = 0
+for i in num:
+    total += int(i)
+print(total)
+
