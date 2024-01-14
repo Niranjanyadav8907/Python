9c588ea5d5b10ce3464aa5fe30c246cd6772c214
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-14 18:12:58 +0000

    print vs returan

---
+++ 71print_returan_function.py
@@ -0,0 +1,18 @@
+# return vs print
+
+def add_three(a,b,c):
+    return a+b+c
+print(add_three(5,5,5))
+
+
+
+#python me return likhkne ki jarurat nhi hain direct print kra skte hain
+
+def add_three(a,b,c):
+    print (a+b+c)
+add_three(5,5,5)
+
+#return is agood kyu kbhi kbhi program print krane keliye kuchh nhi rhta hain only return hota hain
+
+
+
