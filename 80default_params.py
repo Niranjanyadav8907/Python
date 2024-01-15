10eaaea7d5b39182b7043b962d72139d37aa4dcc
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-15 18:55:52 +0000

    Default Parameters

---
+++ 80default_params.py
@@ -0,0 +1,13 @@
+# default parameters
+# default parameter keval last me aap bna skte hain 
+# none python me ek specal vallue hain nane ka mtlb kucch nhi
+
+def user_info(first_name,last_name,age = None ):#  (first_name,last_name ='unknown', age = None):
+    print(f"your first name is {first_name}") 
+    print(f"your last name is {last_name}" )
+    print(f"your age is {age}")
+
+ 
+ 
+user_info('Niranjan', 'yadav',  22)
+ 
