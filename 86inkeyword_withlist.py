caec7e0e2d98a82794dbcc811110ff0ddafd5a3b
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 12:31:49 +0000

    in keyword with list

---
+++ 86inkeyword_withlist.py
@@ -0,0 +1,10 @@
+# hmm kaise  check krenge ki koi elimet hmare list me hain ya nhi
+
+fruits = [ ' orange', 'apple', 'pear,' 'banana', 'kiwi']
+if 'apple' in fruits:
+    print("apple is present")
+else :
+    print("not present")
+    
+
+
