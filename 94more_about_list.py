b181f750de1ff685f784f40d9bb4100697acf395
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 19:27:53 +0000

    More about list

---
+++ 94more_about_list.py
@@ -0,0 +1,33 @@
+## generate list with range functione
+#numbers = list(range(1,11))
+#print(numbers)
+
+
+
+## somthinge more about pop method
+#numbers = list(range(1,11))
+#print(numbers)
+#poppoed_item = numbers.pop ----- pop krke ye return bhi krta hain 
+#numbers.pop()
+#print(numbers)
+
+
+## index method apne list me fine krna hai prticuler number kaha pr hain
+
+#numbers = list(range(1,11))
+#numbers = (1,2,3,4,5,6,7,8,9,10,1)
+#print(numbers.index(1)) #  .index(1,3) ager list me same number 2 baar hai to hmm bta skte hai ki kaha se start krna hai or dusre number ka positione jan skte hain
+
+
+# pass list to a function  hmm apne function ke ander list bhi pass kr skte hain 
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def negative_list(1):
+    negative = []
+    for i in 1:
+        negative.append(-i)
+    return negative
+print(negative_list(numbers))
+
+
+
