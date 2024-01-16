18b421a19fceb56f19eaab74a7c761eeeccf1136
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 19:38:27 +0000

    more about...

--- 94more_about_list.py
+++ 94more_about_list.py
@@ -22,9 +22,9 @@
 # pass list to a function  hmm apne function ke ander list bhi pass kr skte hain 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-def negative_list(1):
+def negative_list(l):
     negative = []
-    for i in 1:
+    for i in l:
         negative.append(-i)
     return negative
 print(negative_list(numbers))
