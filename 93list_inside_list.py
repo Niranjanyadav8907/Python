526313b7de4549df014cf25db68392307c34517b
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 18:40:50 +0000

    List inside list 2D list

---
+++ 93list_inside_list.py
@@ -0,0 +1,28 @@
+# list inside list
+
+matrix = [[1,2,3], [4,5,6], [7,8,9]] #2d list
+## 3 item --- 3 list hain
+#print(matrix[0])
+
+## 3 print krana ho to 3 list
+#for subblist in matrix: # first time loop
+    #for i in subblist: # dobara loop ander walka loop
+        #print(i) 
+
+
+# value ko kaise acsess krenge 
+# for example mujhe 5 chashiye
+
+#print(matrix[1][1]) 
+
+
+#type function in python
+# ye bs  check krne keliye hota hain ki  hmara data kis type ka hain
+
+s = "stringe"
+print(type(s))
+
+
+
+
+
