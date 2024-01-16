d13da085952c648a1bd6c57cd7c4becce9d25381
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 18:12:55 +0000

    List vs String

---
+++ 91string-vs_array.py
@@ -0,0 +1,13 @@
+# list vs strings
+
+# string are immutable (change nhi kr skte hain)
+# list are mutable ()
+
+s = "string" #  print krne pr is  string me change nhi kr skte hain 
+s.title()    # new string bna skte hain
+print(s)
+
+# mutable ka mtlb -- 
+
+l = ['word1', 'word2', 'word3']
+l.pop() # pop metode origanel list me change krega  (pop mtlb remove)
