48c9d146912daa85361c3735022f7f5fc9b78374
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 11:41:44 +0000

    Add data to list

---
+++ 83append_data.py
@@ -0,0 +1,16 @@
+#how to add item to our list
+# most common thing that you can do with your list and most important
+
+fruits = [ 'grapes', 'apple']
+fruits.append('mango') # apned method hmesa list ko last me add krta hain
+print(fruits)
+
+#append method ko real life proggram me kaise use kr skte hain
+
+fruits= []
+fruits.append("mango")
+fruits.append("grapes")
+print(fruits)
+
+
+
