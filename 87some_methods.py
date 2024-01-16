57378aad6150d7818dfe8d41441157016dda1120
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 12:54:44 +0000

    some more list methods

---
+++ 87some_methods.py
@@ -0,0 +1,39 @@
+# count
+# sort methode 
+# sorted function
+# reverse
+# clear
+# copy
+
+
+# count
+fruits = [ ' orange', 'apple', 'pear,' 'banana', 'kiwi']
+print(fruits.count('apple'))
+
+# sort methode  (alpha bitical rup me likhne keliye)
+fruits = [' orange', 'apple', 'pear,' 'banana', 'kiwi']
+fruits.sort()
+print(fruits)
+
+#sorted function
+fruits = [ ' orange', 'apple', 'pear,' 'banana', 'kiwi']
+numbers = [3,5,1,9,10]
+print(sorted(numbers))
+print(fruits)
+
+#clear
+fruits = [ ' orange', 'apple', 'pear,' 'banana', 'kiwi']
+numbers = [3,5,1,9,10]
+numbers.clear()
+print(numbers) # clear krne ke baad number print nhi hoga kyu ki clear ho chuka hain
+
+# copy
+fruits = [ ' orange', 'apple', 'pear,' 'banana', 'kiwi']
+numbers = [3,5,1,9,10]
+numbers_copy = numbers.copy() 
+print(numbers)
+
+
+
+
+
