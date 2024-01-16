f5d71aa1a9c15315711b903f45e04844ca3fe64a
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 17:41:22 +0000

    join and split method

---
+++ 89join_split.py
@@ -0,0 +1,25 @@
+# split method --- ek hi line me user se 2 input krana ho to split method ka use krte hain
+# convert string to list
+
+user_info = 'Niranjan 22 ' .split() # split jaha pr spece hoga vha vha se string ko todh kr string me convert krega 
+print(user_info)
+
+#alg alg do vairibel bhi bna skte hain
+name, age  = input("enter your name and age").split(',')  
+print(name)
+print(age)
+
+
+
+# join method
+# convert list to string
+user_info = ['niranjan', '22']
+print(','.join(user_info)) # ', ' join kr rhe hain comma ke sath
+
+
+
+
+
+
+
+
