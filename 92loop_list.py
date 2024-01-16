36cd7b2796a9d2c32de70794c036c7c60a20be49
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 18:22:50 +0000

    Looping in list

---
+++ 92loop_list.py
@@ -0,0 +1,19 @@
+#list ke ander looping kaise kr skyte hain
+
+fruits = [ 'orange', 'apple', 'pear,' 'banana', 'kiwi']
+
+## foor loop
+
+#for fruits in fruits:
+    #print(fruits)
+
+
+## while loop
+
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i +=1
+
+
+
