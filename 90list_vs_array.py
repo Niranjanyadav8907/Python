5b4cda949a9022984d77486e11f04a92e21f131f
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 18:00:26 +0000

    List vs Array

---
+++ 90list_vs_array.py
@@ -0,0 +1,21 @@
+# list vs array
+
+# array ka mtlb hota hain ordered collection of items
+
+# c,java, c++
+# arry int ,string mtlb array me kuchh phi store kr skte hain
+
+# but python me ye kaam list me krte hain 
+# list - store any data 
+# python ka ek  module hai jisme (array module) hota hai but usme bhi ek hi data type store kr skte ho 
+# but python array module ki speed and proforme kaafi behter hain
+# python me bhi array hai but use hmm numpy array ke jaisa use krte hain
+
+
+# list - store any data this is very  flexible
+
+# javascrit array = python almost same python list
+
+# python ki data since ki jo libry hai wo c me bainding ho rkha jiss se pro.. speed kaafi achha hain
+
+
