9e238d860fbd3ca82fa9f0e3994b5e77d593d8c4
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 12:04:39 +0000

    some more methods to add data in our list, insert method, how to  join(concatenate) two lis , extend method  ,difference between append and extend method

---
+++ 84data_add.py
@@ -0,0 +1,43 @@
+# some more methods to add data in our list
+# insert method
+# how to join(concatenate) two lis 
+# extend method
+# difference between append and extend method
+
+
+# insert method
+#fruits1 = [ ' mango','orange'] 
+#fruits1.insert(1,"grapes") # jis postione pr add krna ho (1,"grapes") aise likhte hain
+#print(fruits1)
+
+# how to join(concatenate) two list
+#fruits1 = [ ' mango','orange'] 
+#fruits2 = [ 'grapes','apple']
+#fruits = fruits1 + fruits2
+#print(fruits)
+
+# extend method
+fruits1 = [ ' mango','orange'] 
+fruits2 = [ 'grapes','apple']
+fruits1.extend(fruits2)
+print(fruits1)
+print(fruits2)
+
+# difference between append and extend method
+fruits1 = [ ' mango','orange'] 
+fruits2 = [ 'grapes','apple']
+fruits1.append(fruits2)
+print(fruits1)
+print(fruits2)   # iasme list ke andr list store hota hain
+
+
+
+
+
+
+
+
+
+
+
+
