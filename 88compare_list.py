4eb8f503e91d5d9a84d9bb424c947efbe379169b
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-16 13:07:50 +0000

    is vs == in list

---
+++ 88compare_list.py
@@ -0,0 +1,14 @@
+# compare list --- list ko compare kaise krte hain
+# == , is 
+
+fruits1 = [ ' orange', 'apple', 'pear,']
+fruits3 = [ ' orange', 'apple', 'pear,']
+fruits2= ['pear,' 'banana', 'kiwi']
+print(fruits1 == fruits2)
+print(fruits1 is fruits3)
+
+# == check krta hain hmare list ka value  same hain ya nhi 
+
+# is  check  krta hai kya hmari list  same place  hmari memori ke ander same jgh pr hai ya nhi 
+
+# value ko check krne keliye == ka use krte hain 
