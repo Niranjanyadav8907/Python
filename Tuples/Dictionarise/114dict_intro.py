6ec0a32b7aafc36fff084243396eafb01d9daf2b
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-17 12:41:57 +0000

    Intro to dictionaries

---
+++ Tuples/Dictionarise/114dict_intro.py
@@ -0,0 +1,16 @@
+#Q - why we use dictionaries?
+# A -Because of limitations of lists, lists are not enough to represent
+# real data.
+
+# Example
+user = ['Harshit', 24, ['coco', 'kimi no na wa'], ['awakening', 'fairy tale']]
+# this list contains user name, age, fav movies, fav tunes
+# you can do this but this is not a good way to do this.
+
+#Q what are dictionaries
+# A unordered collections of data in key value pair.
+
+# how to create dictionaries
+user = {'name': 'Harshit', 'age': 24}
+# print(user)
+# print(type(user))
