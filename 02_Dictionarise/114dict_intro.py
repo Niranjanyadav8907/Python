068e450fcf82cf186e4b8bb7a0e80ceab084a149
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-17 18:04:43 +0000

    Intro to dictionarises

--- 02_Dictionarise/114dict_intro.py
+++ 02_Dictionarise/114dict_intro.py
@@ -3,14 +3,53 @@
 # real data.
 
 # Example
-user = ['Harshit', 24, ['coco', 'kimi no na wa'], ['awakening', 'fairy tale']]
+#user = ['Niranjan', 24, ['coco', 'kimi no na wa'], ['awakening', 'fairy tale']]
+
 # this list contains user name, age, fav movies, fav tunes
 # you can do this but this is not a good way to do this.
 
 #Q what are dictionaries
 # A unordered collections of data in key value pair.
 
-# how to create dictionaries
-user = {'name': 'Harshit', 'age': 24}
-# print(user)
-# print(type(user))
+# how to create dictionaries # pala tarika dic ko creat krne ka
+ 
+#user = {'name': 'Niranjan', 'age': 22}
+#print(user)
+#print(type(user))
+
+# second method to create dictionary
+
+#user1 = dict(name ='Niranjan', age = 22) # idr name me '','' cote lgane ki jarurat nhi hain
+#print(user1)
+
+# how to access data frome dictionary
+# NOTE- there is no indexing because of unorded collectione of data
+# dictionary me data ko accsess krne keliye key ka use krte hain
+
+#for Example 
+
+#user1 = dict(name ='Niranjan', age = 22)
+#print(user['name'])
+
+
+# which type of data a dictionary
+#anthing
+# numbers , stringe, list ,dictionary
+
+#user = ['Niranjan', 24, ['coco', 'kimi no na wa'], ['awakening', 'fairy tale']]
+
+#aap aise bhi ek line me dic create kr skte hain
+
+
+user_info = {
+    'name' :'Niranjan',
+    'age' = 22,
+    'fav_movie' : ['xyz', 'jhhkj', 'uouo'],
+
+}
+print(user_info['fav_movies']) # aise aap key naaam ke sath acsess kr skte hain
+
+
+
+
+
