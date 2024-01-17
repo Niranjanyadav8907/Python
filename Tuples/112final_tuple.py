6ede530d922e24d2133d0cd27dc93a4bbf43e471
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-17 12:28:04 +0000

    Tuple 4th chapter

---
+++ Tuples/112final_tuple.py
@@ -0,0 +1,25 @@
+
+# somthinge more about tuple,list , star
+
+
+# for exaple ek tuple creat krna hain nums naam ko or 1,10 tk number creat krna hain 
+nums = tuple (range(1,11)) 
+print(nums)
+
+# tuple se list me kaise change krte hain
+
+nums =list ((1, 2, 3, 4, 5, 6, 8, 9, 10))
+print(nums)
+
+#tuple se stringe
+nums =str((1, 2, 3, 4, 5, 6, 8, 9, 10))
+# out put jo aa rha uska mtlb ye hai 
+#out put ="(1, 2, 3, 4, 5, 6, 8, 9, 10)""
+print(nums)
+print(type(nums))
+
+
+
+
+
+
