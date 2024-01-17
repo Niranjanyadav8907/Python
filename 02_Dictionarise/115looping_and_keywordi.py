0d3f4ec6856b43e5e00e055daa2d6555c914999d
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-17 18:32:25 +0000

    loopmig

---
+++ 02_Dictionarise/115looping_and_keywordi.py
@@ -0,0 +1,37 @@
+
+# in keyword and iterations in dictionary
+
+user_info = {
+    'name' :'Niranjan',
+    'age': 22,
+    'fav_movie' : ['xyz', 'jhhkj', 'uouo'],
+
+}
+
+
+# check if key exit in dictionary
+if 'name' in user_info
+    print(present)
+else:
+    print('not present')
+
+
+# check if value exist in dictionary ---- values method
+if 22 in user_info.values():
+    print(present)
+else:
+    print('not present')
+
+#loops in dictionaries
+for i in user_info:
+    print(i) # jitne bhi key sare print ho jaynge 
+
+
+
+
+
+
+
+
+
+
