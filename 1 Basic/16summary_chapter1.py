c66e9ea71c71720813bf3edf4557e353450b87a9
Niranjan  Kumar <141658575+Niranjanyadav8907@users.noreply.github.com>
2024-01-18 12:32:11 +0000

    Change

---
+++ 1 Basic/16summary_chapter1.py
@@ -0,0 +1,28 @@
+# *************** summary of chepter one************
+
+#print function
+print('hello "hello" ')
+
+#escape sequence
+print("line A \nline B")
+print("this is backslash  \\\\")
+
+#escape sequence as normal text
+print(r"line A \n line B")
+
+#python as calulater
+print(2+3)
+print(2-3)
+print(2*3)
+print(4/2)  # float divison
+print(4//2) # integer division
+print(2**3) # right to left 2**3**2
+print((2+3) *4 / 2) # 5*4 / 2 = 10
+
+# naming rule for variables 
+name = " niranjan"
+print(name) # variable numeber se start nhi kr skte hain
+
+#convention for variable naming
+user_one = "niranjan"  # snake case writing
+userone = " Niranjan"  # camel case writing
